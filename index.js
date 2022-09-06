71df45bc55aa2d9b1ab8023bb25c88ffb8e84a4a
chase <shao4598@outlook.com>
2022-09-06 19:10:24 +0800

    window改成self

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
+/*
+ * @Author: chase shao4598@outlook.com
+ * @Date: 2022-09-06
+ * @LastEditors: chase shao4598@outlook.com
+ * @LastEditTime: 2022-09-06
+ * @FilePath: /atmosphere.js-node-fork/index.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 var global = (function() { return this; })();
 
 if (typeof global.window === 'undefined') {
     // create a custom window object to emulate the normal browser window
-    var window = {navigator:{userAgent:"atmosphere.js"},document:{},location:{},JSON:JSON};
+    var window = {navigator:{userAgent:"atmosphere-fork.js"},document:{},location:{},JSON:JSON};
 
     window.WebSocket = require("ws");
     window.EventSource = require("eventsource");
@@ -15,4 +23,4 @@ if (typeof global.window === 'undefined') {
     global.document = window.document;
 }
 
-module.exports = require('./lib/atmosphere');
+module.exports = require('./lib/atmosphere-fork');
